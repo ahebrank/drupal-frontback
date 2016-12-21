269790754108774791c501fe0ce1ffa799ec09fc
Andy Hebrank <ahebrank@gmail.com>
2016-12-21 11:34:46 -0500

    update variable organization

--- js/frontback.js
+++ js/frontback.js
@@ -1,13 +1,15 @@
 (function ($, Drupal, drupalSettings) {
 
-  global.frontbackRepo = drupalSettings.frontback.repo_id;
-  global.frontbackPostURL = drupalSettings.frontback.endpoint;
+  window.frontback = {
+    repo: drupalSettings.frontback.repo_id,
+    postUrl: drupalSettings.frontback.endpoint
+  };
   var frontbackVersion = drupalSettings.frontback.version;
 
   var s = document.createElement('script'),
       x = document.getElementsByTagName('script')[0];
   var script = document.createElement('script');
-  script.src = frontbackPostURL + '/assets/js/frontback.js?v=' + frontbackVersion;
+  script.src = frontback.postUrl + '/assets/js/frontback.js?v=' + frontbackVersion;
   document.body.appendChild(script);
 
 })(jQuery, Drupal, drupalSettings);
