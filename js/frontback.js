1f92540971f0bbf07674a05ab4dec29eeed558b7
Andy Hebrank <ahebrank@gmail.com>
2016-12-21 11:13:18 -0500

    10 min cache-busting

--- js/frontback.js
+++ js/frontback.js
@@ -1,12 +1,13 @@
 (function ($, Drupal, drupalSettings) {
 
-  frontbackRepo = drupalSettings.frontback.repo_id;
-  frontbackPostURL = drupalSettings.frontback.endpoint;
-  
+  var frontbackRepo = drupalSettings.frontback.repo_id;
+  var frontbackPostURL = drupalSettings.frontback.endpoint;
+  var frontbackVersion = drupalSettings.frontback.version;
+
   var s = document.createElement('script'),
       x = document.getElementsByTagName('script')[0];
   var script = document.createElement('script');
-  script.src = frontbackPostURL + '/assets/js/frontback.js';
+  script.src = frontbackPostURL + '/assets/js/frontback.js?v=' + frontbackVersion;
   document.body.appendChild(script);
 
 })(jQuery, Drupal, drupalSettings);
