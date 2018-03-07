862e2be029a7f81d62eb651567e78e98396c51cc
Andy Hebrank <ahebrank@gmail.com>
2018-03-07 14:23:42 -0500

    remove attach behavior

--- js/frontback.js
+++ js/frontback.js
@@ -1,16 +1,12 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.frontback = {
-    attach: function (context, settings) {
-      window.frontback = {
-        repo: drupalSettings.frontback.repo_id,
-        postUrl: drupalSettings.frontback.endpoint,
-        options: drupalSettings.frontback.options
-      };
-      var s = document.createElement('script'),
-          x = document.getElementsByTagName('script')[0];
-      var script = document.createElement('script');
-      script.src = frontback.postUrl + '/assets/js/frontback.js?v=' + drupalSettings.frontback.version;
-      document.body.appendChild(script);
-    },
+(function() {
+  window.frontback = {
+    repo: drupalSettings.frontback.repo_id,
+    postUrl: drupalSettings.frontback.endpoint,
+    options: drupalSettings.frontback.options
   };
-})(jQuery, Drupal, drupalSettings);
+  var s = document.createElement('script'),
+      x = document.getElementsByTagName('script')[0];
+  var script = document.createElement('script');
+  script.src = frontback.postUrl + '/assets/js/frontback.js?v=' + drupalSettings.frontback.version;
+  document.body.appendChild(script);
+})();
