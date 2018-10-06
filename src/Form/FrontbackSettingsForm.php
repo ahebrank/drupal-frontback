7a2c33c367b9e061b90a1902355d0394c7bb4612
Andy Hebrank <ahebrank@gmail.com>
2018-10-06 16:41:10 -0400

    Drupal coding standards

--- src/Form/FrontbackSettingsForm.php
+++ src/Form/FrontbackSettingsForm.php
@@ -5,6 +5,9 @@ namespace Drupal\frontback\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Settings form for module.
+ */
 class FrontbackSettingsForm extends ConfigFormBase {
 
   /**
@@ -18,8 +21,8 @@ class FrontbackSettingsForm extends ConfigFormBase {
    * Gets the configuration names that will be editable.
    *
    * @return array
-   * An array of configuration object names that are editable if called in
-   * conjunction with the trait's config() method.
+   *   An array of configuration object names that are editable if called in
+   *   conjunction with the trait's config() method.
    */
   protected function getEditableConfigNames() {
     return ['frontback.admin_settings'];
@@ -30,7 +33,7 @@ class FrontbackSettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $admin_configurations = $this->config('frontback.admin_settings');
-    $form['frontback_repo_id'] = array(
+    $form['frontback_repo_id'] = [
       '#type' => 'textfield',
       '#title' => t('Repo ID'),
       '#default_value' => $admin_configurations->get('frontback_repo_id') ? $admin_configurations->get('frontback_repo_id') : '',
@@ -38,32 +41,32 @@ class FrontbackSettingsForm extends ConfigFormBase {
       '#maxlength' => 60,
       '#description' => t("Homepage for the repo you want to target."),
       '#required' => TRUE,
-    );
-    $form['frontback_endpoint'] = array(
+    ];
+    $form['frontback_endpoint'] = [
       '#type' => 'textfield',
       '#title' => t('Frontback Endpoint URL'),
       '#description' => t("Frontback proxy endpoint"),
       '#default_value' => $admin_configurations->get('frontback_endpoint') ? $admin_configurations->get('frontback_endpoint') : '',
       '#required' => TRUE,
-    );
-    $form['hide_button'] = array(
+    ];
+    $form['hide_button'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide Button'),
       '#description' => t("Hide Feedback button but run script (useful for config split / local dev situations)"),
       '#default_value' => $admin_configurations->get('hide_button'),
-    );
-    $form['hide_assignee_options'] = array(
+    ];
+    $form['hide_assignee_options'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide assignee options'),
       '#description' => t("Disable and hide assignee dropdown (assignee still set in endpoint config)"),
       '#default_value' => $admin_configurations->get('hide_assignee_options'),
-    );
-    $form['hide_reporter_options'] = array(
+    ];
+    $form['hide_reporter_options'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide reporter options'),
       '#description' => t("Disable reporter dropdown; reporter field is text box"),
       '#default_value' => $admin_configurations->get('hide_reporter_options'),
-    );
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -72,17 +75,17 @@ class FrontbackSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config_values = $form_state->getValues();
-    $config_fields = array(
+    $config_fields = [
       'frontback_repo_id',
       'frontback_endpoint',
       'hide_button',
       'hide_assignee_options',
       'hide_reporter_options',
-    );
+    ];
     $config = $this->config('frontback.admin_settings');
     foreach ($config_fields as $config_field) {
       $config->set($config_field, $config_values[$config_field])
-          ->save();
+        ->save();
     }
     parent::submitForm($form, $form_state);
   }
